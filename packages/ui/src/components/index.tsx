a7264a3857e519dc7184e566c4debb2c96ef2527
Sébastien <47519901+SebGazeau@users.noreply.github.com>
2025-05-19 18:25:32 +0200

    add ui components toggle-group and toggle

--- packages/ui/src/components/index.tsx
+++ packages/ui/src/components/index.tsx
@@ -1,5 +1,6 @@
 export { AspectRatio } from "./aspect-ratio";
 export { Avatar, AvatarImage, AvatarFallback } from "./avatar";
+export { Badge } from "./badge";
 export {
   Breadcrumb,
   BreadcrumbItem,
@@ -9,18 +10,57 @@ export {
   BreadcrumbSeparator,
 } from "./breadcrumb";
 export { Button } from "./button";
-
+export { Calendar } from "./calendar";
+export {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+} from "./card";
+export { ChartContainer, ChartTooltip, ChartTooltipContent } from "./chart";
+export type { ChartConfig } from "./chart";
 export { Checkbox } from "./checkbox";
-
+export {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "./collapsible";
+export {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+  DialogFooter,
+} from "./dialog";
 export {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "./dropdown-menu";
+export {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./form";
 export { Input } from "./input";
 export { Label } from "./label";
 export { Logo } from "./logo";
+export {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "./pagination";
+export { Popover, PopoverContent, PopoverTrigger } from "./popover";
 export {
   Select,
   SelectContent,
@@ -35,6 +75,14 @@ export {
 } from "./select";
 export { Separator } from "./separator";
 export {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetDescription,
+  SheetFooter,
+} from "./sheet";
+export {
   Sidebar,
   SidebarContent,
   SidebarHeader,
@@ -46,7 +94,7 @@ export {
   SidebarFooter,
   SidebarProvider,
 } from "./sidebar";
-export { Badge } from "./badge";
+export { Switch } from "./switch";
 export {
   Table,
   TableBody,
@@ -55,56 +103,8 @@ export {
   TableHeader,
   TableRow,
 } from "./table";
-export { ChartContainer, ChartTooltip, ChartTooltipContent } from "./chart";
-export type { ChartConfig } from "./chart";
-export { Popover, PopoverContent, PopoverTrigger } from "./popover";
-export { Calendar } from "./calendar";
-export {
-  Pagination,
-  PaginationContent,
-  PaginationEllipsis,
-  PaginationItem,
-  PaginationLink,
-  PaginationNext,
-  PaginationPrevious,
-} from "./pagination";
-export {
-  Dialog,
-  DialogContent,
-  DialogHeader,
-  DialogTitle,
-  DialogDescription,
-  DialogFooter,
-} from "./dialog";
-export {
-  Sheet,
-  SheetContent,
-  SheetHeader,
-  SheetTitle,
-  SheetDescription,
-  SheetFooter,
-} from "./sheet";
-export {
-  Collapsible,
-  CollapsibleContent,
-  CollapsibleTrigger,
-} from "./collapsible";
 export { Tabs, TabsList, TabsTrigger, TabsContent } from "./tabs";
-export { Switch } from "./switch";
-export {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "./form";
 export { Textarea } from "./textarea";
-export {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-  CardDescription,
-} from "./card";
+export { ToggleGroup, ToggleGroupItem } from "./toggle-group";
+export { Toggle, toggleVariants } from "./toggle";
 export { Tooltip, TooltipContent, TooltipTrigger } from "./tooltip";
