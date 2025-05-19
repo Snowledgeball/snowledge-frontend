e6a28153cbae1dd57ebba70b7ccf8bd54c5fde08
Sébastien <47519901+SebGazeau@users.noreply.github.com>
2025-05-19 21:42:03 +0200

    skeleton page settings notification

---
+++ apps/frontend/components/settings/notifications/title.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+export default function NotificationTitle() {
+    return (
+        <div className="space-y-2">
+            <h1 className="text-xl md:text-2xl font-bold text-foreground">
+                Notifications
+            </h1>
+            <p className="text-sm text-muted-foreground">
+                Stay informed with customizable notifications! Choose how to
+                receive updates, so you never miss important info.
+            </p>
+        </div>
+    );
+}
